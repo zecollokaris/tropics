207b16a01d27f28fac4ec9416cf651992f728736
zecollokaris <collo.kariss@gmail.com>
2018-05-30 12:17:56 +0300

    set up database

--- project/settings.py
+++ project/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tropics',
+        'USER': 'karis',
+        'PASSWORD': 'Kar!s123',
     }
 }
 
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
